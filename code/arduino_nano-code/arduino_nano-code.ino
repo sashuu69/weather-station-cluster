4e8331302d8d62b70a426467be46cf3e819fc213
Sashwat K <sashwat0001@gmail.com>
2020-12-28 09:27:20 +0530

    completed arduino nano code

--- code/arduino_nano-code/arduino_nano-code.ino
+++ code/arduino_nano-code/arduino_nano-code.ino
@@ -2,7 +2,7 @@
  * Project Name: Home Weather Station Mini
  * Program Name: Arduino Nano code
  * Created on: 13/12/2020 08:04:00 PM
- * Last Modified: 23/12/2020 02:47:00 PM
+ * Last Modified: 28/12/2020 09:15:00 PM
  * Created by: Sashwat K
  */
 
@@ -36,23 +36,13 @@ RTC_DS1307 rtc;
 
 void setup() {
   // put your setup code here, to run once:
-//  Serial.begin(9600);
-//  Serial.println("\n=========================");
-//  Serial.println("Home Weather Station Mini");
-//  Serial.println("------ Arduino Nano -------");
-//  Serial.println("---------------------------");
-//  Serial.println("=========================\n");
-
-//  Serial.println("*************************");
-//  Serial.println("*************************");
+  Serial.begin(9600);
   Serial.println("Initialising sensors...");
   
   // Initialise DHT22
-//  Serial.println("1. Initialising DHT22..");
   dht.begin();
   
   // Initialise BMP280
-//  Serial.println("2. Initialising BMP280..");
   int bmp_lib = bmp.begin();
   if(!bmp_lib){
     Serial.println("\t BMP init failed!");
@@ -60,7 +50,6 @@ void setup() {
     abort();
   }
   else {
-//    Serial.println("\t BMP init success!");
     bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
                     Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
                     Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
@@ -69,34 +58,30 @@ void setup() {
   }
 
   // Initialise RTC module
-//  Serial.println("3. Initialising RTC..");
   if (! rtc.begin()) {
     Serial.println("\t Couldn't find RTC");
     Serial.flush();
     abort();
   }
   if (! rtc.isrunning()) {
-//    Serial.println("\t RTC is NOT running, let's set the time!");
+    Serial.println("\t RTC is NOT running, let's set the time!");
     rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
   }
 
   // Initialise rain sensor
-//  Serial.println("4. Initialising rain sensor");
   pinMode(INPUT, RAINSENSORPIN);
   
-//  Serial.println("5. Initialising rain guage");
+  // Initialise rain guage
   pinMode(INPUT, RAINGUAGEPIN);
 
-  s_serial_to_esp.begin(4800); // Begin software Serial
+  // Begin software Serial
+  s_serial_to_esp.begin(4800);
   
-//  Serial.println("initialization complete.");
-  Serial.println("*************************");
-  Serial.println("*************************\n\n\n");
+  Serial.println("initialization complete...\n");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.println("---------------------------\n");
 
   // Read data from DHT22
   float dht_humidity = dht.readHumidity();
@@ -106,13 +91,16 @@ void loop() {
   // Read data from BMP280
   double bmp_temperature, bmp_pressure, bmp_altitude;
   bmp_temperature = bmp.readTemperature();
-  bmp_pressure = bmp.readPressure();
-  bmp_altitude = bmp.readAltitude(1013.25);
+  /* bmp_pressure = pressure_value / 100 + const
+   * const = 1018.33 - (get pressure value of your location from https://en.allmetsat.com/metar-taf/)
+   */
+  bmp_pressure = bmp.readPressure() / 100.00 + 5.33;
+  bmp_altitude = bmp.readAltitude(bmp_pressure);
 
   // Read data from RTC module
-  DateTime time = rtc.now();
-  String rtc_date = time.timestamp(DateTime::TIMESTAMP_DATE);
-  String rtc_time = time.timestamp(DateTime::TIMESTAMP_TIME);
+  DateTime now = rtc.now();
+  String rtc_date = now.toString("DD-MM-YYYY");
+  String rtc_time = now.toString("hh:mm:ss");
 
   // Read data from Rain Sensor
   int rain_sensor_data = analogRead(RAINSENSORPIN);
@@ -132,28 +120,10 @@ void loop() {
   doc["bmp_altitude"] = bmp_altitude;
   doc["rain_sensor_data"] = rain_sensor_data;
   doc["rain_guage_data"] = rain_guage_data;
-  serializeJson(doc, s_serial_to_esp); // Send JSON via Serial
 
-  // Print info
-//  Serial.println("1. DHT22 Data:-");
-//  Serial.print("\t i. Humidity: "); Serial.println(dht_humidity);
-//  Serial.print("\t ii. Temperature: "); Serial.println(dht_temperature);
-//  Serial.print("\t iii. Heat Index: "); Serial.println(dht_heat_index);
-//
-//  Serial.println("2. BMP280 Data:-");
-//  Serial.print("\t i. Pressure: "); Serial.println(bmp_pressure);
-//  Serial.print("\t ii. Altitude: "); Serial.println(bmp_altitude);
-//  Serial.print("\t iii. Temperature: "); Serial.println(bmp_temperature);
-//
-//  Serial.println("3. RTC module Data:-");
-//  Serial.print("\t i. Timestamp Date: "); Serial.println(rtc_date);
-//  Serial.print("\t ii. Timestamp Time: "); Serial.println(rtc_time);
-//
-//  Serial.println("4. Rain Sensor Data:-");
-//  Serial.print("\t i. Value: ");Serial.println(rain_sensor_data);
-//
-//  Serial.println("5. Rain Guage Data:-");
-//  Serial.print("\t i. Value: ");Serial.println(rain_guage_data);
+  // Send JSON via Serial
+  serializeJson(doc, s_serial_to_esp);
+  Serial.println("Loop complete..");
 
   delay(1000);
 }
