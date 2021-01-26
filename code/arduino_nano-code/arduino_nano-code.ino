b3abb3d10900aaabb048c65c315e5605c635d1ec
Sashwat K <sashwat0001@gmail.com>
2021-01-26 21:02:40 +0530

    Updated Mega Code

--- code/arduino_nano-code/arduino_nano-code.ino
+++ code/arduino_nano-code/arduino_nano-code.ino
@@ -1,8 +1,9 @@
 /*
  * Project Name: Home Weather Station Mini
- * Program Name: Arduino Nano code
+ * Program Name: Arduino Mega code
+ * Board Name : Arduino Mega
  * Created on: 13/12/2020 08:04:00 PM
- * Last Modified: 28/12/2020 09:15:00 PM
+ * Last Modified: 26/01/2021 09:03:00 PM
  * Created by: Sashwat K
  */
 
@@ -10,11 +11,11 @@
 #include "ArduinoJson.h"
 
 #include <SoftwareSerial.h> // Library for Software Serial
-SoftwareSerial s_serial_to_esp(6, 7); //RX, TX
+SoftwareSerial s_serial_to_esp(11, 12); //RX, TX
 
 // DHT22
 #include "DHT.h"
-#define DHTPIN 3
+#define DHTPIN 5
 #define DHTTYPE DHT22
 DHT dht(DHTPIN, DHTTYPE);
 
@@ -32,28 +33,34 @@ RTC_DS1307 rtc;
 #include <SPI.h>
 #include <SD.h>
 File sd_card;
+int sdcard_chip_select = 53;
 
 // Rain Sensor
-#define RAINSENSORPIN A0
+#define RAINSENSORPIN A7
 
 // Rain Guage
-#define RAINGUAGEPIN 4
+#define RAINGUAGEPIN 7
 
 // Global counter
 int counter = 0;
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(115200);
-  Serial.println("Initialising sensors...");
+  Serial.begin(9600);
+  Serial.println("Home Weather Station Mini");
+  Serial.println("-------------------------");
+  Serial.println("Sensor Initialisation \n");
   
   // Initialise DHT22
+  Serial.print("1. Initialising DHT22");
   dht.begin();
-  
+  Serial.println("\t Success");
+
   // Initialise BMP280
+  Serial.print("2. Initialising BMP280");
   int bmp_lib = bmp.begin();
   if(!bmp_lib){
-    Serial.println("\t BMP init failed!");
+    Serial.println("\t Failed");
     Serial.flush();
     abort();
   }
@@ -63,35 +70,51 @@ void setup() {
                     Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
                     Adafruit_BMP280::FILTER_X16,      /* Filtering. */
                     Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
+    Serial.println("\t Success");
   }
 
   // Initialise RTC module
+  Serial.print("3. Initialising RTC module");
   if (! rtc.begin()) {
-    Serial.println("\t Couldn't find RTC");
+    Serial.println("\t Failed");
     Serial.flush();
     abort();
+  }
+  else {
+    Serial.println("\t Success");
   }
   if (! rtc.isrunning()) {
     Serial.println("\t RTC is NOT running, let's set the time!");
     rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
   }
 
-  if (!SD.begin(4)) {
-    Serial.println("initialization failed!");
+  Serial.print("4. Initialising Micro SD card module");
+  pinMode(sdcard_chip_select, OUTPUT);
+  if (!SD.begin()) {
+    Serial.println("\t Failed");
     while (1);
   }
-  sd_card = SD.open("sensor_data.txt", FILE_WRITE);
+  else {
+    Serial.println("\t Success");
+  }
 
+  Serial.print("5. Initialising Rain sensor");
   // Initialise rain sensor
   pinMode(INPUT, RAINSENSORPIN);
-  
+  Serial.println("\t Success");
+
+  Serial.print("5. Initialising Rain guage");
   // Initialise rain guage
   pinMode(INPUT, RAINGUAGEPIN);
+  Serial.println("\t Success");
 
   // Begin software Serial
+  Serial.print("6. Initialising software serial");
   s_serial_to_esp.begin(4800);
+  Serial.println("\t Success");
   
-  Serial.println("initialization complete...\n");
+  Serial.println("\ninitialization complete");
+  Serial.println("-------------------------\n");
 }
 
 void loop() {
@@ -138,14 +161,28 @@ void loop() {
   serializeJson(doc, s_serial_to_esp);
   doc.clear();
 
+  // Store data in micro sdcard every 30 seconds
   if (counter % 30 == 0) {
-    counter = 0;
     String final_string = rtc_date + "," + rtc_time + "," + dht_humidity + "," + dht_temperature + "," + dht_heat_index + "," + bmp_temperature + "," + bmp_pressure + "," + bmp_altitude + "," + rain_sensor_data + "," + rain_guage_data;
+    sd_card = SD.open("sensor_data.txt", FILE_WRITE);
     sd_card.println(final_string);
+    sd_card.close();
+    counter = 0;
   }
-  else {
-    counter ++;
-  }
-  Serial.println("Loop complete..");
+  counter ++;
+
+  // Serial print for debugging
+  Serial.println("-----------------------------------");
+  Serial.print("RTC Date: "); Serial.println(rtc_date);
+  Serial.print("RTC Time: "); Serial.println(rtc_time);
+  Serial.print("DHT22 Humidity: "); Serial.print(dht_humidity); Serial.println(" %");
+  Serial.print("DHT22 Temperature: "); Serial.print(dht_temperature); Serial.println(" °C");
+  Serial.print("BMP280 Temperature: "); Serial.print(bmp_temperature); Serial.println(" °C");
+  Serial.print("BMP280 Pressure: "); Serial.print(bmp_pressure); Serial.println(" mbar");
+  Serial.print("BMP Altitude: "); Serial.print(bmp_altitude); Serial.println(" m");
+  Serial.print("Rain Sensor: "); Serial.println(rain_sensor_data);
+  Serial.print("Rain Guage: "); Serial.println(rain_guage_data);
+  Serial.println("-----------------------------------\n");
+  
   delay(1000);
 }
